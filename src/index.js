895a71c584789be893bbc3b26db390829fdc4028
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2020-01-28 02:20:24 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+$(".menu__item").click(function(e) {
+    $(".menu__box").css({ "display" : "block" });
+    $(".menu__box").css({ "visibility" : "hidden" });
+    $(".menu__box").css({ "left" : "-100px" });
+});
